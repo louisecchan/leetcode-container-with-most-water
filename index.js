c8e82a214472c60f0f9e5d88daa76e881998da17
Louise <louise.cch@gmail.com>
2023-11-09 18:44:40 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+// You are given an integer array height of length n.
+// There are n vertical lines drawn such that
+// the two endpoints of the ith line
+// are (i, 0) and (i, height[i]).
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+
+var maxArea = function (height) {
+  let left = 0; // Left pointer starting from the leftmost edge
+  let right = height.length - 1; // Right pointer starting from the rightmost edge
+  let maxWater = 0; // Initialize the maximum water capacity
+
+  while (left < right) {
+    // Calculate the width of the container
+    let width = right - left;
+
+    // Calculate the height of the container (the minimum height between the two lines)
+    let h = Math.min(height[left], height[right]);
+
+    // Calculate the water capacity of the current container
+    let water = width * h;
+
+    // Update the maximum water capacity if the current container holds more water
+    maxWater = Math.max(maxWater, water);
+
+    // Move the pointers towards each other
+    if (height[left] < height[right]) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+
+  return maxWater;
+};
